8cdda631f43c3820597360fb608f59cbbb2a6ffb
mandorakannu <mandorakannu@gmail.com>
2023-10-01 23:36:25 +0530

    Add Images

---
+++ components/shared/Images.tsx
@@ -0,0 +1,45 @@
+import community from "@images/community.svg";
+import create from "@images/create.svg";
+import deleteImg from "@images/delete.svg";
+import edit from "@images/edit.svg";
+import heartFilled from "@images/heart-filled.svg";
+import heartGray from "@images/heart-gray.svg";
+import heart from "@images/heart.svg";
+import home from "@images/home.svg";
+import logo from "@images/logo.svg";
+import logout from "@images/logout.svg";
+import members from "@images/members.svg";
+import more from "@images/more.svg";
+import profile from "@images/profile.svg";
+import reply from "@images/reply.svg";
+import repost from "@images/repost.svg";
+import request from "@images/request.svg";
+import searchGray from "@images/search-gray.svg";
+import search from "@images/search.svg";
+import share from "@images/share.svg";
+import tag from "@images/tag.svg";
+import user from "@images/user.svg";
+
+export {
+  community,
+  create,
+  deleteImg,
+  edit,
+  heartFilled,
+  heartGray,
+  heart,
+  home,
+  logo,
+  logout,
+  members,
+  more,
+  profile,
+  reply,
+  repost,
+  request,
+  searchGray,
+  search,
+  share,
+  tag,
+  user,
+};
