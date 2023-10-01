3530b3c872bc64305a256b4615e7efcb2f0982e2
mandorakannu <mandorakannu@gmail.com>
2023-10-01 22:57:50 +0530

    Sign-In page align centered

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function SignInPage() {
-  return <SignIn />;
+  return<section className="grid place-items-center py-20"><SignIn /></section>;
 }
